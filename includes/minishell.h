06f7d8b3d82ce960682374887d2502b29577adbc
Byrd Betsey <bbetsey12@gmail.com>
2021-05-08 23:36:23 +0300

    add pipe & semicolon support

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,28 +6,30 @@
 
 typedef struct s_env
 {
-	char			*name;
-	char			*value;
-	struct s_env	*next;
-	struct s_env	*prev;
-}					t_env;
+	char				*name;
+	char				*value;
+	struct s_env		*next;
+	struct s_env		*prev;
+}						t_env;
 
 typedef struct s_cmd
 {
-	char			*cmd;
-	char			**flags;
-	int				argc;
-	char			**argv;
-	struct s_cmd	*next;
-}					t_cmd;
+	char				*cmd;
+	char				**flags;
+	int					argc;
+	char				**argv;
+	int					pipe;
+	struct s_cmd		*next;
+}						t_cmd;
 
 typedef struct s_cmdtable
 {
-	char			*input_file;
-	char			*output_file;
-	int				cmds_count;
-	t_cmd			*cmds;
-}					t_cmdtable;
+	char				*input_file;
+	char				*output_file;
+	int					cmds_count;
+	t_cmd				*cmds;
+	struct s_cmdtable	*next;
+}						t_cmdtable;
 
 int			get_next_line(int fd, char **line);
 
